9049549debbe19af74841fa8ef2bd68b79ccb235
luke.murphy6 <minen3335@gmail.com>
2021-08-04 11:35:09 +0800

    First Commit

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-menu = ("Cappuccino","Espresso","Latte","Iced Coffee")
+menu = ["Cappuccino","Espresso","Latte","Iced Coffee"]
+price = [3.00,2.25,2.50,2.50]
 
 Order_item = []
 Price_item = []
@@ -9,21 +10,25 @@ while more_orders:
     if ordermethod== "New Order":
         order = input("What would you like?: ")
         if order == "Cappuccino":
-            cap_quantity = input("How many would you like?: ")
+            cap_quantity = int(input("How many would you like?: "))
             Order_item.append(menu[0] + " * " + str(cap_quantity))
+            Price_item.append(price[0] * (cap_quantity))
         elif order == "Espresso":
-            esp_quantity = input("How many would you like?: ")
+            esp_quantity = int(input("How many would you like?: "))
             Order_item.append(menu[1] + " * " + str(esp_quantity))
+            Price_item.append(price[1] * (esp_quantity))
         elif order == "Latte":
-            lat_quantity = input("How many would you like?: ")
+            lat_quantity = int(input("How many would you like?: "))
             Order_item.append(menu[2] + " * " + str(lat_quantity))
+            Price_item.append(price[2] * (lat_quantity))
         elif order == "Iced Coffee":
-            ice_quantity = input("How many would you like?: ")
+            ice_quantity = int(input("How many would you like?: "))
             Order_item.append(menu[3] + " * " + str(ice_quantity))
+            Price_item.append(price[3] * (ice_quantity))
         else:
             print("Wrong Input")
     next_item = input("Next item?: (Y/N)")
     if next_item == "N":
         more_orders = False
 print(Order_item)
-print()
+print(f"${Price_item}")
